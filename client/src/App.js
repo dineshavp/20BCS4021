b05f8da7a17c589cc7c9d23e04e9f5831ac62de1
Dinesh P <dineshavp@gmail.com>
2023-10-06 13:28:25 +0530

    route updtaed

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import SharedLayout from './Components/Dashboard/SharedLayout';
 import RegisterCompany from './Components/Auth/RegisterCompany';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/register' element={<RegisterCompany/>}/> 
-        {/* <Route path='/signup' element={<Signup/>}/>        
-        <Route path='/dashboard' element={<SharedLayout/>}>
-          <Route index element={<Home/>}/>
-          <Route path='test' element={<Tests/>}/>
-        </Route> */}
+        <Route path='/' element={<RegisterCompany/>}/> 
+        
       </Routes>
     </BrowserRouter>
   );
